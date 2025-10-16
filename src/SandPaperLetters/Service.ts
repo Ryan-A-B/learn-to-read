31ea6b337aeadc90ba77a191d4dd583e119dc369
rballinger <rballinger@topcon.com>
2025-10-16 16:52:05 +1000

    adds ability to change letters

--- src/SandPaperLetters/Service.ts
+++ src/SandPaperLetters/Service.ts
@@ -20,7 +20,7 @@ interface Proxy {
 
 abstract class AbstractState {
     abstract readonly name: string;
-    initialise = (proxy: Proxy, canvas: [HTMLCanvasElement, HTMLCanvasElement]): void => { }
+    initialise = (proxy: Proxy, input: InitialiseInput): void => { }
     animate = (proxy: Proxy): void => { }
     handle_resize = (proxy: Proxy, event: UIEvent): void => { }
     handle_mouse_down = (proxy: Proxy, event: MouseEvent): void => { }
@@ -35,24 +35,26 @@ abstract class AbstractState {
 class NotInitialised extends AbstractState {
     readonly name = "NotInitialised";
 
-    initialise = (proxy: Proxy, canvas: [HTMLCanvasElement, HTMLCanvasElement]): void => {
-        const guide_layer = new Layer(canvas[0]);
-        const drawing_layer = new Layer(canvas[1]);
+    initialise = (proxy: Proxy, input: InitialiseInput): void => {
+        const guide_layer = new Layer(input.guide_canvas);
+        const drawing_layer = new Layer(input.drawing_canvas);
+        const ui_layer = new Layer(input.ui_canvas);
 
         window.addEventListener("resize", proxy.handle_resize);
-        drawing_layer.canvas.addEventListener("mousedown", proxy.handle_mouse_down);
-        drawing_layer.canvas.addEventListener("mousemove", proxy.handle_mouse_move);
-        drawing_layer.canvas.addEventListener("mouseup", proxy.handle_mouse_up);
-        drawing_layer.canvas.addEventListener("touchstart", proxy.handle_touch_start, { passive: false });
-        drawing_layer.canvas.addEventListener("touchmove", proxy.handle_touch_move, { passive: false });
-        drawing_layer.canvas.addEventListener("touchend", proxy.handle_touch_end, { passive: false });
-        drawing_layer.canvas.addEventListener("touchcancel", proxy.handle_touch_cancel, { passive: false });
+        window.addEventListener("mousedown", proxy.handle_mouse_down);
+        window.addEventListener("mousemove", proxy.handle_mouse_move);
+        window.addEventListener("mouseup", proxy.handle_mouse_up);
+        window.addEventListener("touchstart", proxy.handle_touch_start, { passive: false });
+        window.addEventListener("touchmove", proxy.handle_touch_move, { passive: false });
+        window.addEventListener("touchend", proxy.handle_touch_end, { passive: false });
+        window.addEventListener("touchcancel", proxy.handle_touch_cancel, { passive: false });
 
         const letterIndex = Math.floor(Math.random() * 26);
         const letter = letters[letterIndex];
 
         proxy.set_state(new Ready({
             guide_layer,
+            ui_layer,
             drawing_layer,
             letter,
         }));
@@ -61,6 +63,7 @@ class NotInitialised extends AbstractState {
 
 interface NewReadyInput {
     guide_layer: Layer;
+    ui_layer: Layer;
     drawing_layer: Layer;
     letter: Letter;
 }
@@ -69,37 +72,75 @@ class Ready extends AbstractState {
     readonly name = "Ready";
     private readonly guide_layer: Layer;
     private readonly drawing_layer: Layer;
+    private readonly ui_layer: Layer;
     private readonly mask_layer: OffscreenLayer;
+    private readonly refresh_button: RefreshButton;
     private readonly letter: Letter;
 
     constructor(input: NewReadyInput) {
         super();
         this.guide_layer = input.guide_layer;
         this.drawing_layer = input.drawing_layer;
+        this.ui_layer = input.ui_layer;
         this.letter = input.letter;
-        this.mask_layer = new OffscreenLayer(new OffscreenCanvas(this.guide_layer.canvas.width, this.guide_layer.canvas.height));
+        this.mask_layer = new OffscreenLayer(new OffscreenCanvas(this.ui_layer.canvas.width, this.ui_layer.canvas.height));
 
         const text_align: CanvasTextAlign = "center";
         const text_baseline: CanvasTextBaseline = "middle";
-        
+
         this.guide_layer.clear();
         this.guide_layer.context.textAlign = text_align;
         this.guide_layer.context.textBaseline = text_baseline;
         this.guide_layer.context.fillStyle = "#aaaaaa";
-        
+
         this.mask_layer.clear();
         this.mask_layer.context.textAlign = text_align;
         this.mask_layer.context.textBaseline = text_baseline;
 
-        const landscape = this.guide_layer.canvas.width > this.guide_layer.canvas.height;
+        const landscape = this.ui_layer.canvas.width > this.ui_layer.canvas.height;
         if (landscape) this.prepare_landscape();
         else this.prepare_portrait();
+
+        this.ui_layer.clear();
+
+        this.ui_layer.context.strokeStyle = "black";
+        this.ui_layer.context.lineWidth = 14;
+        this.ui_layer.context.lineCap = "round";
+
+        const refresh_button_size = Math.min(this.ui_layer.canvas.width, this.ui_layer.canvas.height) / 8;
+        this.refresh_button = new RefreshButton({
+            layer: this.ui_layer,
+            position: [
+                this.ui_layer.canvas.width - refresh_button_size * 3 / 4,
+                refresh_button_size * 3 / 4
+            ],
+            size: refresh_button_size,
+        });
+        this.refresh_button.render();
     }
 
     handle_mouse_down = (proxy: Proxy, event: MouseEvent): void => {
+        const left_click = event.button === 0;
+        if (!left_click) return;
+
+        const position = this.ui_layer.get_position([event.clientX, event.clientY]);
+        const hit = this.refresh_button.test_hit(position);
+        if (hit) {
+            const letterIndex = Math.floor(Math.random() * 26);
+            const letter = letters[letterIndex];
+            proxy.set_state(new Ready({
+                guide_layer: new Layer(this.guide_layer.canvas),
+                ui_layer: new Layer(this.ui_layer.canvas),
+                drawing_layer: new Layer(this.drawing_layer.canvas),
+                letter: letter,
+            }));
+        }
+
         proxy.set_state(new MouseDrawing({
             guide_layer: this.guide_layer,
             drawing_layer: this.drawing_layer,
+            ui_layer: this.ui_layer,
+            refresh_button: this.refresh_button,
             mask_layer: this.mask_layer,
             letter: this.letter,
             mouse_event: event,
@@ -110,9 +151,29 @@ class Ready extends AbstractState {
     handle_touch_start = (proxy: Proxy, event: TouchEvent): void => {
         event.preventDefault();
         if (event.touches.length === 0) return;
+
+        if (event.touches.length === 1) {
+            const touch = event.touches[0];
+            const position = this.ui_layer.get_position([touch.clientX, touch.clientY]);
+            const hit = this.refresh_button.test_hit(position);
+            if (hit) {
+                const letterIndex = Math.floor(Math.random() * 26);
+                const letter = letters[letterIndex];
+                proxy.set_state(new Ready({
+                    guide_layer: new Layer(this.guide_layer.canvas),
+                    ui_layer: new Layer(this.ui_layer.canvas),
+                    drawing_layer: new Layer(this.drawing_layer.canvas),
+                    letter: letter,
+                }));
+                return;
+            }
+        }
+
         proxy.set_state(new TouchDrawing({
             guide_layer: this.guide_layer,
             drawing_layer: this.drawing_layer,
+            ui_layer: this.ui_layer,
+            refresh_button: this.refresh_button,
             mask_layer: this.mask_layer,
             letter: this.letter,
             touch_event: event,
@@ -123,13 +184,14 @@ class Ready extends AbstractState {
     handle_resize = (proxy: Proxy, event: UIEvent): void => {
         proxy.set_state(new Ready({
             guide_layer: new Layer(this.guide_layer.canvas),
+            ui_layer: new Layer(this.ui_layer.canvas),
             drawing_layer: new Layer(this.drawing_layer.canvas),
             letter: this.letter,
         }));
     }
 
     private prepare_landscape = (): void => {
-        const font_size = Math.round(this.guide_layer.canvas.height * 0.6);
+        const font_size = Math.round(this.ui_layer.canvas.height * 0.6);
         const font = `${font_size}px monospace`;
         const text = `${this.letter.upper}${this.letter.lower}`;
 
@@ -137,29 +199,29 @@ class Ready extends AbstractState {
         this.mask_layer.context.font = font;
 
         const center: Vec2 = [
-            this.guide_layer.canvas.width / 2,
-            this.guide_layer.canvas.height / 2
+            this.ui_layer.canvas.width / 2,
+            this.ui_layer.canvas.height / 2
         ];
         this.guide_layer.context.fillText(text, center[0], center[1]);
         this.mask_layer.context.fillText(text, center[0], center[1]);
     }
 
     private prepare_portrait = (): void => {
-        const font_size = Math.round(this.guide_layer.canvas.height * 15 / 36);
+        const font_size = Math.round(this.ui_layer.canvas.height * 15 / 36);
         const font = `${font_size}px monospace`;
         this.guide_layer.context.font = font;
         this.mask_layer.context.font = font;
 
         const upper_center: Vec2 = [
-            this.guide_layer.canvas.width / 2,
-            this.guide_layer.canvas.height * 9 / 36
+            this.ui_layer.canvas.width / 2,
+            this.ui_layer.canvas.height * 9 / 36
         ];
         this.guide_layer.context.fillText(this.letter.upper, upper_center[0], upper_center[1]);
         this.mask_layer.context.fillText(this.letter.upper, upper_center[0], upper_center[1]);
 
         const lower_center: Vec2 = [
-            this.guide_layer.canvas.width / 2,
-            this.guide_layer.canvas.height * 23 / 36
+            this.ui_layer.canvas.width / 2,
+            this.ui_layer.canvas.height * 23 / 36
         ];
         this.guide_layer.context.fillText(this.letter.lower, lower_center[0], lower_center[1]);
         this.mask_layer.context.fillText(this.letter.lower, lower_center[0], lower_center[1]);
@@ -169,6 +231,8 @@ class Ready extends AbstractState {
 interface NewDrawingInput {
     guide_layer: Layer;
     drawing_layer: Layer;
+    ui_layer: Layer;
+    refresh_button: RefreshButton;
     mask_layer: OffscreenLayer;
     letter: Letter;
     position: Vec2;
@@ -177,6 +241,8 @@ interface NewDrawingInput {
 abstract class Drawing extends AbstractState {
     protected readonly guide_layer: Layer;
     protected readonly drawing_layer: Layer;
+    protected readonly ui_layer: Layer;
+    protected readonly refresh_button: RefreshButton;
     protected readonly mask_layer: OffscreenLayer;
     protected readonly letter: Letter;
     protected last_position: Vec2;
@@ -186,6 +252,8 @@ abstract class Drawing extends AbstractState {
         super();
         this.guide_layer = input.guide_layer;
         this.drawing_layer = input.drawing_layer;
+        this.ui_layer = input.ui_layer;
+        this.refresh_button = input.refresh_button;
         this.mask_layer = input.mask_layer;
         this.letter = input.letter;
         this.position = input.position;
@@ -218,6 +286,8 @@ abstract class Drawing extends AbstractState {
 interface NewMouseDrawingInput {
     guide_layer: Layer;
     drawing_layer: Layer;
+    ui_layer: Layer;
+    refresh_button: RefreshButton;
     mask_layer: OffscreenLayer;
     letter: Letter;
     mouse_event: MouseEvent;
@@ -231,6 +301,8 @@ class MouseDrawing extends Drawing {
         super({
             guide_layer: input.guide_layer,
             drawing_layer: input.drawing_layer,
+            ui_layer: input.ui_layer,
+            refresh_button: input.refresh_button,
             mask_layer: input.mask_layer,
             letter: input.letter,
             position: position,
@@ -245,6 +317,8 @@ class MouseDrawing extends Drawing {
         proxy.set_state(new Clearing({
             guide_layer: this.guide_layer,
             drawing_layer: this.drawing_layer,
+            ui_layer: this.ui_layer,
+            refresh_button: this.refresh_button,
             letter: this.letter,
         }));
     }
@@ -253,6 +327,8 @@ class MouseDrawing extends Drawing {
 interface NewTouchDrawingInput {
     guide_layer: Layer;
     drawing_layer: Layer;
+    ui_layer: Layer;
+    refresh_button: RefreshButton;
     mask_layer: OffscreenLayer;
     letter: Letter;
     touch_event: TouchEvent;
@@ -268,6 +344,8 @@ class TouchDrawing extends Drawing {
         super({
             guide_layer: input.guide_layer,
             drawing_layer: input.drawing_layer,
+            ui_layer: input.ui_layer,
+            refresh_button: input.refresh_button,
             mask_layer: input.mask_layer,
             letter: input.letter,
             position: position,
@@ -291,6 +369,8 @@ class TouchDrawing extends Drawing {
         proxy.set_state(new Clearing({
             guide_layer: this.guide_layer,
             drawing_layer: this.drawing_layer,
+            ui_layer: this.ui_layer,
+            refresh_button: this.refresh_button,
             letter: this.letter,
         }));
     }
@@ -302,6 +382,8 @@ class TouchDrawing extends Drawing {
         proxy.set_state(new Clearing({
             guide_layer: this.guide_layer,
             drawing_layer: this.drawing_layer,
+            ui_layer: this.ui_layer,
+            refresh_button: this.refresh_button,
             letter: this.letter,
         }));
     }
@@ -319,6 +401,8 @@ class TouchDrawing extends Drawing {
 interface NewClearingInput {
     guide_layer: Layer;
     drawing_layer: Layer;
+    ui_layer: Layer;
+    refresh_button: RefreshButton;
     letter: Letter;
 }
 
@@ -326,19 +410,24 @@ class Clearing extends AbstractState {
     readonly name = "Clearing";
     private readonly guide_layer: Layer;
     private readonly drawing_layer: Layer;
+    private readonly ui_layer: Layer;
+    private readonly refresh_button: RefreshButton;
     private readonly letter: Letter;
 
     constructor(input: NewClearingInput) {
         super();
         this.guide_layer = input.guide_layer;
         this.drawing_layer = input.drawing_layer;
+        this.ui_layer = input.ui_layer;
+        this.refresh_button = input.refresh_button;
         this.letter = input.letter;
     }
 
     animate = (proxy: Proxy): void => {
-        this.drawing_layer.context.clearRect(0, 0, this.drawing_layer.canvas.width, this.drawing_layer.canvas.height);
+        this.drawing_layer.context.clearRect(0, 0, this.ui_layer.canvas.width, this.ui_layer.canvas.height);
         proxy.set_state(new Ready({
             guide_layer: this.guide_layer,
+            ui_layer: this.ui_layer,
             drawing_layer: this.drawing_layer,
             letter: this.letter,
         }));
@@ -346,6 +435,12 @@ class Clearing extends AbstractState {
 }
 
 type State = NotInitialised | Ready | MouseDrawing | TouchDrawing | Clearing;
+
+interface InitialiseInput {
+    guide_canvas: HTMLCanvasElement;
+    ui_canvas: HTMLCanvasElement;
+    drawing_canvas: HTMLCanvasElement;
+}
 
 class Service {
     private state: State;
@@ -367,8 +462,8 @@ class Service {
         };
     }
 
-    public initialise(canvas: [HTMLCanvasElement, HTMLCanvasElement]): void {
-        this.state.initialise(this.proxy, canvas);
+    public initialise(input: InitialiseInput): void {
+        this.state.initialise(this.proxy, input);
     }
 
     private animate = (): void => {
@@ -415,3 +510,67 @@ class Service {
 }
 
 export default Service;
+
+interface NewRefreshButtonInput {
+    layer: Layer;
+    position: Vec2;
+    size: number;
+}
+
+class RefreshButton {
+    private readonly layer: Layer;
+    private readonly position: Vec2;
+    private readonly diameter: number;
+    private readonly radius: number;
+    private readonly bounding_box: [Vec2, Vec2];
+
+    constructor(input: NewRefreshButtonInput) {
+        this.layer = input.layer;
+        this.position = input.position;
+        this.diameter = input.size;
+        this.radius = this.diameter / 2;
+
+        this.bounding_box = [
+            [this.position[0] - this.radius, this.position[1] - this.radius],
+            [this.position[0] + this.radius, this.position[1] + this.radius],
+        ];
+    }
+
+    render = () => {
+        const angle_offset = Math.PI * 1 / 6;
+        const start_angle = angle_offset;
+        const end_angle = 2 * Math.PI - angle_offset;
+        this.layer.context.beginPath();
+        this.layer.context.arc(this.position[0], this.position[1], this.radius, start_angle, end_angle);
+        this.layer.context.stroke();
+
+        const arrow_size = this.radius * 2 / 6;
+        const point_position: Vec2 = [
+            this.position[0] + this.radius * Math.cos(end_angle),
+            this.position[1] + this.radius * Math.sin(end_angle),
+        ];
+        const line_angle = Math.PI / 6;
+        const line_angle_offset = Math.PI * 47 / 36;
+        const line_start: Vec2 = [
+            point_position[0] + arrow_size * Math.cos(line_angle_offset + line_angle),
+            point_position[1] + arrow_size * Math.sin(line_angle_offset + line_angle),
+        ];
+        const line_end: Vec2 = [
+            point_position[0] + arrow_size * Math.cos(line_angle_offset - line_angle),
+            point_position[1] + arrow_size * Math.sin(line_angle_offset - line_angle),
+        ];
+        this.layer.context.beginPath();
+        this.layer.context.moveTo(line_start[0], line_start[1]);
+        this.layer.context.lineTo(point_position[0], point_position[1]);
+        this.layer.context.lineTo(line_end[0], line_end[1]);
+        this.layer.context.stroke();
+    }
+
+    test_hit = (point: Vec2): boolean => {
+        if (point[0] < this.bounding_box[0][0]) return false;
+        if (point[0] > this.bounding_box[1][0]) return false;
+        if (point[1] < this.bounding_box[0][1]) return false;
+        if (point[1] > this.bounding_box[1][1]) return false;
+        return true;
+    }
+}
--- src/SandPaperLetters/index.tsx
+++ src/SandPaperLetters/index.tsx
@@ -8,17 +8,24 @@ const service = new Service();
 const SandPaperLetters: React.FunctionComponent = () => {
     const guide_canvas_ref = React.useRef<HTMLCanvasElement | null>(null);
     const drawing_canvas_ref = React.useRef<HTMLCanvasElement | null>(null);
+    const ui_canvas_ref = React.useRef<HTMLCanvasElement | null>(null);
 
     React.useLayoutEffect(() => {
-        if (guide_canvas_ref.current === null) throw new Error("canvas 1 not set")
-        if (drawing_canvas_ref.current === null) throw new Error("canvas 2 not set")
-        service.initialise([guide_canvas_ref.current, drawing_canvas_ref.current])
+        if (guide_canvas_ref.current === null) throw new Error("guide canvas not set")
+        if (drawing_canvas_ref.current === null) throw new Error("drawing canvas not set")
+        if (ui_canvas_ref.current === null) throw new Error("ui canvas not set")
+        service.initialise({
+            guide_canvas: guide_canvas_ref.current,
+            drawing_canvas: drawing_canvas_ref.current,
+            ui_canvas: ui_canvas_ref.current,
+        });
     }, []);
 
     return (
         <div id="sand-paper-letters" className="viewport">
             <canvas ref={guide_canvas_ref}></canvas>
             <canvas ref={drawing_canvas_ref}></canvas>
+            <canvas ref={ui_canvas_ref}></canvas>
         </div>
     )
 }
